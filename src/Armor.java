3c839600b9b7aa0bc066675542a9b6f8a00b2f13
YuriyVoytovych <yuriyvoytovych1310@gmail.com>
2023-05-29 12:53:21 +0300

    Initial commit

---
+++ src/Armor.java
@@ -0,0 +1,1 @@
+public class Armor {    String name;    int price;    int def;    int set_lvl;    public Armor(String armor_name,int armor_price, int armor_def, int access_lvl){         name=armor_name;         price=armor_price;         def=armor_def;         set_lvl=access_lvl;    }}
---
+++ src/Weapon.java
@@ -0,0 +1,1 @@
+public class Weapon {    String name;    int lvl;    int price;    int damage;    public Weapon(String w_name, int access_lvl, int w_price, int w_damage){        name=w_name;        lvl=access_lvl;        price=w_price;        damage=w_damage;    }}
